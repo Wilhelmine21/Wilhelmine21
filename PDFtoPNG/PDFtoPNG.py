ebedb67619d7ca3b948afb3019a287becb97bb9f
Ting-Yu Chen <89498643+Wilhelmine21@users.noreply.github.com>
2022-06-15 14:48:23 +0800

    Update PDFtoPNG.py

--- PDFtoPNG/PDFtoPNG.py
+++ PDFtoPNG/PDFtoPNG.py
@@ -2,4 +2,4 @@ YourFileName="tmp"+".pdf"
 from pdf2image import convert_from_path
 images = convert_from_path(YourFileName)
 for i in range(len(images)):
-    images[i].save(YourFileName+'_page'+ str(i+1) +'.png', 'PNG')
+    images[i].save(YourFileName[:-4]+'_page'+ str(i+1) +'.png', 'PNG')
