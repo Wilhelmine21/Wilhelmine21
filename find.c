b58a9ecf4bbfa73ceee86a8ad0b38baded7ff367
Ting-Yu Chen <89498643+Wilhelmine21@users.noreply.github.com>
2023-02-11 20:39:17 +0800

    Update find.c

--- find.c
+++ find.c
@@ -5,42 +5,49 @@
 
 #define MAX_LINE_LENGTH 256
 
-int main() {
-    clock_t t1 = clock();
-
-    FILE *f1 = fopen("inst.txt", "r");
-    FILE *f2 = fopen("report.txt", "r");
-    FILE *out = fopen("output.log", "w");
-
-    char inst[MAX_LINE_LENGTH][MAX_LINE_LENGTH];
-    int inst_count = 0;
-    char line[MAX_LINE_LENGTH];
-    bool found;
-
-    while (fgets(line, MAX_LINE_LENGTH, f1)) {
-        line[strcspn(line, "\n")] = 0;
-        strcpy(inst[inst_count++], line);
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s finst freport\n", argv[0]);
+        return 1;
     }
 
-    while (fgets(line, MAX_LINE_LENGTH, f2)) {
-        line[strcspn(line, "\n")] = 0;
-        found = false;
-        for (int i = 0; i < inst_count; i++) {
-            if (strstr(line, inst[i])) {
-                fprintf(out, "%s\n", inst[i]);
-                found = true;
-                break;
-            }
+    char *finst = argv[1];
+    char *freport = argv[2];
+
+    clock_t t1 = clock();
+    FILE *f1 = fopen(finst, "r");
+    FILE *f2 = fopen(freport, "r");
+    FILE *out = fopen("output.log", "w");
+
+    if (!f1 || !f2 || !out) {
+        printf("Error opening files\n");
+        return 1;
+    }
+
+    bool inst[MAX_LINE_LENGTH];
+    memset(inst, false, sizeof(inst));
+
+    char line[MAX_LINE_LENGTH];
+    int count = 0;
+    while (fgets(line, MAX_LINE_LENGTH, f1)) {
+        int len = strlen(line);
+        if (line[len - 1] == '\n') {
+            line[len - 1] = '\0';
         }
-        if (found) {
-            for (int i = 0; i < inst_count; i++) {
-                if (strcmp(line, inst[i]) == 0) {
-                    for (int j = i; j < inst_count - 1; j++) {
-                        strcpy(inst[j], inst[j + 1]);
-                    }
-                    inst_count--;
-                    break;
-                }
+        inst[count++] = true;
+    }
+
+    count = 0;
+    while (fgets(line, MAX_LINE_LENGTH, f2)) {
+        int len = strlen(line);
+        if (line[len - 1] == '\n') {
+            line[len - 1] = '\0';
+        }
+        for (int i = 0; i < count; i++) {
+            if (inst[i] && strstr(line, line + i * MAX_LINE_LENGTH)) {
+                fprintf(out, "%s\n", line + i * MAX_LINE_LENGTH);
+                inst[i] = false;
+                break;
             }
         }
     }
@@ -49,10 +56,11 @@ int main() {
     fclose(f2);
     fclose(out);
 
-    double ta = (double)(clock() - t1) / CLOCKS_PER_SEC;
-    int minutes = (int)ta / 60;
-    double seconds = ta - minutes * 60;
-    printf("It took %d min %.2f sec.\n", minutes, seconds);
+    clock_t ta = clock() - t1;
+    double elapsed_time = (double) ta / CLOCKS_PER_SEC;
+    int minutes = (int) elapsed_time / 60;
+    double seconds = elapsed_time - minutes * 60;
+    printf("It cost %d min %.2f sec.\n", minutes, seconds);
 
     return 0;
 }
